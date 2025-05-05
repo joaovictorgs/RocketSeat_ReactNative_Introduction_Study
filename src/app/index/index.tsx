829c0580c49005193952e5d9f8927f20c5a6815d
joaovictorgs <joaovictorgodoydasilva@gmail.com>
2025-05-05 12:45:23 -0300

    adicao da primeira parte do curso

---
+++ src/app/index/index.tsx
@@ -0,0 +1,69 @@
+import {
+  Image,
+  View,
+  TouchableOpacity,
+  FlatList,
+  Modal,
+  Text,
+} from "react-native";
+
+import { MaterialIcons } from "@expo/vector-icons";
+
+import { styles } from "./styles";
+import { colors } from "@/styles/colors";
+
+import { Link } from "@/components/link";
+import { Categories } from "@/components/categories";
+import { Option } from "@/components/option";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={require("@/assets/logo.png")} style={styles.logo} />
+        <TouchableOpacity>
+          <MaterialIcons name="add" size={32} color={colors.green[300]} />
+        </TouchableOpacity>
+      </View>
+      <Categories />
+
+      <FlatList
+        data={["1", "2", "3", "4", "5"]}
+        keyExtractor={(item) => item}
+        renderItem={() => (
+          <Link
+            name="Rocketseat"
+            url="https://rocketseat.com.br"
+            onDetails={() => console.log("clicoi")}
+          ></Link>
+        )}
+        style={styles.links}
+        contentContainerStyle={styles.linksContent}
+        showsVerticalScrollIndicator={false}
+      ></FlatList>
+
+      <Modal transparent visible={true}>
+        <View style={styles.modal}>
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalCategory}>Curso</Text>
+              <TouchableOpacity>
+                <MaterialIcons
+                  name="close"
+                  size={20}
+                  color={colors.gray[400]}
+                />
+              </TouchableOpacity>
+            </View>
+            <Text style={styles.modalLinkName}>Rocekseat</Text>
+            <Text style={styles.modalUrl}>https://www.rocketseat.com.br</Text>
+            <View style={styles.modalFooter}>
+              <Option name="Excluir" icon="delete" variant="secondary"></Option>
+              <Option name="Abrir" icon="language"></Option>
+            </View>
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+}
